d7ec29e5b725f1ad34de56482af35b4660171bf3
Mart <Mart@dhcp-39-60-64.dhcpint.csulb.edu>
2018-11-11 14:50:34 -0800

    Chapter 2 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome to Tour of Heroes';
+  title = 'Tour of Heroes';
 
 }
